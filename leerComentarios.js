d62b5aae9755492ce6e0cfc9e7cfe7bdeab196f3
Nicolas Perez <51518394+NicolasPerezUNLaSMN@users.noreply.github.com>
2022-10-01 17:52:40 -0300

    Web lista para iniciar el curso
    Solo faltan links de tareas

---
+++ leerComentarios.js
@@ -0,0 +1,35 @@
+﻿
+const vue = new Vue({
+ el:'#app',
+ data:{
+  listaDatos : []
+ },
+ created(){
+  this.getLista()
+ },
+ methods:{
+  recargar(){
+   console.log("recargando");
+   this.getLista()
+  },
+getLista(){
+
+ // id de la hoja de calculo
+ idSheets = '1QP23FT1i_TxSC2ZIcy71cSP4CSfu-iP0NH1DNZ3vmFo';
+ //// nuestra      APIKey
+ apiKey = 'AIzaSyAHMfYpGI4dFVbFwT9ak7Gq9CeTedROWHA'; 
+ // rango de la hoja de calculo que queremos leer
+ values = 'A2:AZ100';
+// fetch es un método nativo para hacer peticiones http
+// en el navegador 
+ fetch("https://content-sheets.googleapis.com/v4/spreadsheets/" +   idSheets + "/values/A2:AZ100?access_token="+ apiKey +"&key="+  apiKey)
+.then((lista)=>{
+  return lista.json()
+}).then((valores)=>{
+  this.listaDatos = valores.values
+}).catch(err=>{
+  console.log(err);
+})
+} // fin funcion getLista()
+} // fin methods
+}) // fin instancia
